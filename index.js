a9a2791a882f322b3efa6d5ff4fc728e832f4b12
Surya <77264555+surya-nagrajan@users.noreply.github.com>
2022-03-14 15:33:26 +0530

    finalll

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const port=process.env.PORT || 4000;
 app.get("/", (req, res) => {
   res.send("progress");
 });
-
+app.get("/all", (req, res) => {
+  res.send("progress");
+});
 app.post("/get-values",(req, res, ) => {
   console.log(req.body);
 });
 
 
 app.listen(port,()=>{
-  console.log("i am listening on port")
+  console.log("i am listening on port");
 });
 
 
